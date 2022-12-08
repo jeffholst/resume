f6178a01022be53a51dd08513c500cbdd615e542
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2022-12-08 16:53:33 -0600

    chore: favicon and logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,43 @@
 export default {
   title: `Jeff Holst`,
   description: `Jeff's personal website`,
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "site.webmanifest" }],
+    ["link", { rel: "shortcut icon", href: "favicon.ico" }],
+  ],
+  lastUpdated: true,
   themeConfig: {
     editLink: {
       pattern: "https://github.com/jeffholst/resume/edit/main/docs/:path",
       text: "Edit this page on GitHub",
     },
-    logo: "/JH.svg",
+    logo: "/logo.svg",
     nav: [
       { text: "Contact", link: "Contact" },
       { text: "Résumé", link: "Resume" },
